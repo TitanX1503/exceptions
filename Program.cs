f25252eb03969ce02029c23c2e17e0c0509ee99b
TitanX1503 <60981537+TitanX1503@users.noreply.github.com>
2020-06-25 10:32:27 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Error <---> Exception
+
+            int result;
+
+            int a = 10;
+            int b = 0;
+
+            Calculator calculator = new Calculator();
+
+            try {
+                
+            System.Console.WriteLine($"{a} / {b} = {calculator.divide(a, b)}");
+                } catch(DivideByZeroException e) {
+                //Manejar excepción
+                System.Console.WriteLine("Error: división entre 0");
+            }
+
+
+            try
+            {
+                Bus bus = new Bus();
+
+                bus.SetDriverName("Gabriel Herrera");
+                bus.SetRoute("R");
+                bus.BeginRoute();
+
+                System.Console.WriteLine("Bus is ok");
+                // System.Console.WriteLine("Bus execution complete.");
+            }
+            catch (ArgumentException e)
+            {
+                System.Console.WriteLine("Invalid Bus params");
+                System.Console.WriteLine(e.Message);
+
+                // System.Console.WriteLine("Bus execution complete.");
+            } catch (InvalidOperationException e) {
+                System.Console.WriteLine("Invalid Bus state");
+                System.Console.WriteLine(e.Message);
+
+                // System.Console.WriteLine("Bus execution complete.");
+            } finally {
+                System.Console.WriteLine("Bus execution complete.");
+            }
+        }
+    }
+}
